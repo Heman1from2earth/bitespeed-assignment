763ab8cd1f1566ddb358482c160a23cb07d121fe
Heman1from2earth <140407033+Heman1from2earth@users.noreply.github.com>
2025-06-03 05:16:17 +0530

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const db = mysql.createPool({
   port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = db
