a15c3fe99d4c6c17ccb3d82e7d7a517c964ee76a
Heman1from2earth <140407033+Heman1from2earth@users.noreply.github.com>
2025-06-03 06:50:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const identifyRoute = require("./routes/identify");
 require("dotenv").config();
 
 app.use(express.json());
-app.use("/", identifyRoute);
+app.use("/identify", identifyRoute);
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
